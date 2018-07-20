48d99a9295c44a13eca5a1c96b17964b758efcbc
korwin <levozavr@mail.ru>
2018-07-20 15:15:38 +0300

    learn rca to kill himself refactor some code where fix small bugs

--- env/Unity_client.py
+++ env/Unity_client.py
@@ -7,9 +7,10 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(('localhost', 9090))
 
 while True:
-    name = input('name')
-    cmd = input('cmd')
-    dataToSend = {"flag": 0, "name": str(name), "command": str(cmd)}
+    flag = input('flag: ')
+    name = input('name: ')
+    cmd = input('cmd: ')
+    dataToSend = {"flag": flag, "name": str(name), "command": str(cmd)}
     dataJson = json.dumps(dataToSend)
     sock.send(dataJson.encode())
     time.sleep(0.04)
